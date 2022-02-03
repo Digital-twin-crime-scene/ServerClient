c036892e77b322ade8b7af8aa70cdfbad216c349
Artysh Chaybar <arty1901@gmail.com>
2022-02-03 15:20:29 +0700

    Init Commit

---
+++ FileMonitor/Program.cs
@@ -0,0 +1,6 @@
+using FileMonitor;
+
+CreateHostBuilder(args).Build().Run();
+
+static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
